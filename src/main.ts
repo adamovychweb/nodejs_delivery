e6e9d0e4449118f59e1013a98533b22ece7aadf2
Bohdan Adamovych <bohdan.wknow11@gmail.com>
2022-05-05 14:11:00 +0300

    add basic docs page

--- src/main.ts
+++ src/main.ts
@@ -1,13 +1,23 @@
 import { NestFactory } from '@nestjs/core';
 import * as cookieParser from 'cookie-parser';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+import { ValidationPipe } from '@nestjs/common';
 
 import { AppModule } from './app.module';
-import { ValidationPipe } from '@nestjs/common';
 import { AppConfigService } from '~core/config/services/app-config.service';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   const appConfig = app.get(AppConfigService);
+
+  const swaggerConfig = new DocumentBuilder()
+    .setTitle('Delivery company API')
+    .setDescription('Delivery company API with drivers and customers')
+    .setVersion('1.0')
+    .addTag('User')
+    .build();
+  const swaggerDocument = SwaggerModule.createDocument(app, swaggerConfig);
+  SwaggerModule.setup('api-docs', app, swaggerDocument);
 
   app.setGlobalPrefix('api');
   app.use(cookieParser());
@@ -15,6 +25,7 @@ async function bootstrap() {
   app.useGlobalPipes(new ValidationPipe({ transform: true }));
   await app.listen(appConfig.port);
   console.log(`App started on port: ${appConfig.port}`);
+  console.log(`API docs: http://localhost:${appConfig.port}/api-docs`);
 }
 
 bootstrap();
